c2971b6a29af23b72e0ecd79cc1c4cdf42e6caa4
Vishal Jadoun <work.vishaljadoun@gmail.com>
2026-08-19 18:16:07 +0530

    inital navbar look (#3)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import potLogo from '../../assets/pot.svg';
 import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | 
-      <Link to="/recipes">Recipes</Link> | 
-      <Link to="/planner">Planner</Link> | 
-      <Link to="/shopping">Shopping</Link> | 
-      <Link to="/dashboard">Dashboard</Link> | 
-      <Link to="/login">Login</Link>
+    <nav className='navbar'>
+      <div className='navbarContainer'>
+
+        <Link to="#" className='navbarLogo'>
+          <span className='logoIcon'><img src={potLogo} alt="CleanCook Logo" /></span>
+          <span className='logoText'>CleanCook</span>
+        </Link>
+
+        <ul className='navbarLinks'>
+
+          <li><NavLink to="" className="navItem">Home</NavLink></li>
+          <li><NavLink to="/recipe" className="navItem">Recipe</NavLink></li>
+          <li><NavLink to="/planner" className="navItem">Planner</NavLink></li>
+          <li><NavLink to="/shopping" className="navItem">Shopping</NavLink></li>
+        </ul>
+
+        <div className='navbarActions'>
+          <Link to="/login" className='loginBtn'>Login</Link>
+        </div>
+      </div>
     </nav>
   );
 }
