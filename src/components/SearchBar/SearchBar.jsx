dd0fd346aa6d327fd9030fea757d74d739b562ed
Vishal Jadoun <work.vishaljadoun@gmail.com>
2026-08-23 16:00:33 +0530

    remaning features in component (#10)

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import './SearchBar.css';
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search..." />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
