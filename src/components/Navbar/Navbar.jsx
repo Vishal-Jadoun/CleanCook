8bc0a5ab2fdc8ff790a7eea5bd0058fc77edfeee
Vishal Jadoun <work.vishaljadoun@gmail.com>
2026-08-23 17:42:50 +0530

    removed login button , added profile icon as dasboard redirection

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import { Link, NavLink } from 'react-router-dom';
 import potLogo from '../../assets/pot.svg';
 import './Navbar.css';
-
+import userIcon from "../../assets/user.svg";
 function Navbar() {
   return (
     <nav className='navbar'>
       <div className='navbarContainer'>
 
-        <Link to="#" className='navbarLogo'>
+        <Link to="/" className='navbarLogo'>
           <span className='logoIcon'><img src={potLogo} alt="CleanCook Logo" /></span>
           <span className='logoText'>CleanCook</span>
         </Link>
@@ -18,12 +18,18 @@ function Navbar() {
           <li><NavLink to="/recipe" className="navItem">Recipe</NavLink></li>
           <li><NavLink to="/planner" className="navItem">Planner</NavLink></li>
           <li><NavLink to="/shopping" className="navItem">Shopping</NavLink></li>
+          {/* <li><NavLink to="/dashboard" className="navItem">Dashboard</NavLink></li> */}
+
         </ul>
 
         <div className='navbarActions'>
-          <Link to="/login" className='loginBtn'>Login</Link>
+          {/* <Link to="/login" className='loginBtn'>Login</Link>  login and singup will be added in section evaluation  */}
+          <Link to="/dashboard" className='profileBtn'>
+            <img src={userIcon} alt="Profile" className='profileSvg' />
+          </Link>
         </div>
       </div>
+
     </nav>
   );
 }
