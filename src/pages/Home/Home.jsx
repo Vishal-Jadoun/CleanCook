e0fcce6dc22923a70f2776b3b3667365b85d8baf
Vishal Jadoun <work.vishaljadoun@gmail.com>
2026-08-23 22:38:08 +0530

    adding feature cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import './Home.css';
 import heroImg from "../../assets/hero.png";
+import cal from "../../assets/calendar.png";
+import chef from "../../assets/chef.png";
+import chart from "../../assets/chart.png";
+import target from "../../assets/target.png";
 
 function Home() {
   return (
@@ -22,6 +26,32 @@ function Home() {
           </div>
 
         </div>
+
+        <div className='featureBarWrap'>
+            <div className='featureContainer'>
+
+              <div className='featureCard'>
+                <span className='featureIcon'><img src={cal} alt="calander" /></span>
+                <h4>Meal Planning</h4>
+                <p>Plan your nutritious meals for the week effortlessly</p>
+              </div>
+              <div className='featureCard'>
+                <span className='featureIcon'><img src={chef} alt="calander" /></span>
+                <h4>Recipe Discovery</h4>
+                <p>Find new, healthy, and delicious recipes daily</p>
+              </div>
+              <div className='featureCard'>
+                <span className='featureIcon'><img src={chart} alt="calander" /></span>
+                <h4>Nutrition Tracking</h4>
+                <p>Track your calories, protein, and daily water intak</p>
+              </div>
+              <div className='featureCard'>
+                <span className='featureIcon'><img src={target} alt="calander" /></span>
+                <h4>Wellness Goals</h4>
+                <p>Set targets and hit your personal fitness goals</p>
+              </div>
+            </div>
+          </div>
 
 
     </section>
