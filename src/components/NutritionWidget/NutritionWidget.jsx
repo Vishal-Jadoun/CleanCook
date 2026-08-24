37781d73da30fcc0155dab94b9850c71b61cb711
Vishal Jadoun <work.vishaljadoun@gmail.com>
2026-08-24 20:22:51 +0530

    Addy dashboard (#24)
    * Merge addy-nutritionwidget into main
    
    * Merge addy-nutritionwidget into main
    
    * dashboard
    
    ---------
    
    Co-authored-by: Aditya Bansal <ifakeaddy0@gmail.com>

--- src/components/NutritionWidget/NutritionWidget.jsx
+++ src/components/NutritionWidget/NutritionWidget.jsx
@@ -3,7 +3,7 @@ import './NutritionWidget.css';
 function NutritionWidget() {
   return (
     <div className="nutrition-widget">
-{/* Header */}
+      <div className="nutrition-header">
 <div className="nutrition-header">
         <div className="nutrition-icon">
           🥗
@@ -13,7 +13,6 @@ function NutritionWidget() {
           <p>Track your calories & macros</p>
         </div>
       </div>
-      {/* Calories Section */}
       <div className="nutrition-calories-box">
         <div className="calories-text">
           <span className="cal-label">🔥 Calories</span>
@@ -23,9 +22,9 @@ function NutritionWidget() {
           <div className="progress-fill cal-bar"></div>
         </div>
       </div>
-      {/* Macro Nutrients Breakdown */}
+ 
       <div className="macro-list">
-        {/* Protein */}
+     
         <div className="macro-item">
           <div className="macro-info">
             <span>🍗 Protein</span>
@@ -35,7 +34,7 @@ function NutritionWidget() {
             <div className="progress-fill protein-bar"></div>
           </div>
         </div>
-        {/* Carbs */}
+      
         <div className="macro-item">
           <div className="macro-info">
             <span>🍞 Carbs</span>
@@ -45,7 +44,7 @@ function NutritionWidget() {
             <div className="progress-fill carbs-bar"></div>
           </div>
         </div>
-        {/* Fat */}
+     
         <div className="macro-item">
           <div className="macro-info">
             <span>🥑 Fat</span>
